79a171feff03ee5581c7b1b7e47dddac9f71c726
Augusto <gutopssouza@gmail.com>
2019-07-29 16:03:11 -0300

    :rocket: Create class component TechList and configure babel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import TechList from "./components/TechList";
+
 import banner from "./assets/testebannermeetapp.jpg";
 
 function App() {
-  return <img src={banner} />;
+  return <TechList />;
 }
 
 export default App;
